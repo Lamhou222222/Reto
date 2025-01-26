b24194894cb70ad5973199b9683bb6eb8586546b
tuerjfg@jhrfbgj.con <lamhour@gmail.com>
2025-01-27 00:00:48 +0100

    jbrtgk

--- src/Proyecto/Reto.java
+++ src/Proyecto/Reto.java
@@ -5,7 +5,7 @@ public class Reto {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello the world");
-		System.out.println("Hello the second time joijo bro");
+		System.out.println("Hello the second time joijo5tgojn45oentgoij5nrjtgniutn2 bro");
 	}
 
 }
