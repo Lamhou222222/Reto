5e9d28f1cc1b14e2845e9b718233350e29e905ae
tuerjfg@jhrfbgj.con <lamhour@gmail.com>
2025-01-26 23:24:42 +0100

    the first commit

---
+++ src/Proyecto/Reto.java
@@ -0,0 +1,11 @@
+package Proyecto;
+
+public class Reto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello the world");
+		System.out.println("Hello the second time");
+	}
+
+}
